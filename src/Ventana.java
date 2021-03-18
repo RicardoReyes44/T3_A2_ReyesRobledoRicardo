44e5bd9a014b9605875a24626e329b1d29b70a24
RicardoReyes44 <rickyboy1002@gmail.com>
2021-03-18 16:41:25 -0600

    Se agregaron los textarea 'area1', 'area2' y 'txt'

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,18 +1,32 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 
-public class Ventana extends JFrame{
+public class Ventana extends JFrame implements ActionListener{
 
 	public Ventana(){
 		getContentPane().setLayout(new FlowLayout());
 		setTitle("Tarea de concurrencia");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		JTextArea area1 = new JTextArea();
+		JTextArea area2 = new JTextArea();
 		
+		JLabel txt = new JLabel(" ");
 		
 		setVisible(true);
 		setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
