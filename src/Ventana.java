ba74cbb099b87524940f7d86f4c1b78cfddeff7e
RicardoReyes44 <rickyboy1002@gmail.com>
2021-03-18 15:57:24 -0600

    Se agrego el layout, posicion, titulo y se condiguro el boton de cerrar y visibilidad en la clase 'Ventana'

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,9 +1,18 @@
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
+
 
 public class Ventana extends JFrame{
 
 	public Ventana(){
+		getContentPane().setLayout(new FlowLayout());
+		setTitle("Tarea de concurrencia");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		
+		
+		setVisible(true);
+		setLocationRelativeTo(null);
 	}
 	
 }
